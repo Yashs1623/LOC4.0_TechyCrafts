5f7396f6067b5785967e2e4a5667a360b3216222
Abhishek93248 <abhishek93248@gmail.com>
2022-03-12 15:29:30 +0530

    done nothing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AuthState from './context/AuthCheck/AuthState';
 import NGO from './components/NGO';
 
 
+
 function App() {
   return (
    
@@ -27,6 +28,7 @@ function App() {
                 <Route exact path='/ngo' element={<NGO/> }></Route> 
             </Routes>
         </div>
+       
         </Router>
       </AuthState> 
     </>
